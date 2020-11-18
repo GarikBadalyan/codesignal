e5d566568ff3af93d34518df5cf34660f0f81954
GarikBadalyan <74115207+GarikBadalyan@users.noreply.github.com>
2020-11-18 23:13:47 +0300

    codesignal 4 problem

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
 
-function polindrom(str) {
-  return str == str.split("").reverse().join("");
-}
-console.log(polindrom("asert"));
 
+let arr1 = [2,4,-6,45,2,-4,6]
+function adj(inputArr){
+  let largestRezult =inputArr[0]*inputArr[1]
+  for(let i = 0;i<inputArr.length-1;i++){
+    if(inputArr[i]*inputArr[i+1] > largestRezult){
+      largestRezult = inputArr[i]*inputArr[i+1]
+    }
+  } 
+  return largestRezult
+}
+console.log( adj(arr1))
 
 
 
